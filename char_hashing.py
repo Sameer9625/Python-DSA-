0d3773e5615670b23f09d67a6b865dede733a9b1
Sameer <sameerlntmhipower@gmail.com>
2026-02-22 11:20:44 +0530

    Add files via upload

---
+++ char_hashing.py
@@ -0,0 +1,14 @@
+s = "azyxyyzaaaa"
+q = ["d", "a", "y", "x"]
+#Method 1
+# for ch in q:
+#     ascii_val = ord(ch)
+#     index = ascii_val-97
+#     hash_list[index] += 1
+
+# for ch in q:
+#     ascii_val = ord(ch)
+#     index = ascii_val-97
+#     print(hash_list[index])
+
+    
